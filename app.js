486fbcf9940c921e4e26559dc9b5f770f4584a59
SaimHasanD <saimhasan514@gmail.com>
2022-03-02 22:02:56 +0600

    total cost

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-function handleNumberChange(isIncrease) {
-    const inputNumber = document.getElementById('firstClassInput');
-    const newNumber = parseInt(inputNumber.value);
-    let totalNumber = newNumber;
+// Add event listener
+function handleNumberChange(ticket, isIncrease) {
+    const ticketInput = document.getElementById(ticket + 'Input');
+    const newTicket = parseInt(ticketInput.value);
+    let totalTicket = newTicket;
     if (isIncrease == true) {
-        totalNumber = newNumber + 1;
+        totalTicket = newTicket + 1;
     }
-    if (isIncrease == false) {
-        totalNumber = newNumber - 1;
+    if (isIncrease == false && totalTicket > 0) {
+        totalTicket = newTicket - 1;
     }
-    document.getElementById('firstClassInput').value = totalNumber;
+    document.getElementById(ticket + 'Input').value = totalTicket;
+    calculateTotal();
+}
+
+// calculate total cost
+function calculateTotal(){
+    const firstClassTicket = getTicketNumber('firstClass')
+    const economyTicket = getTicketNumber('economy')
+
+    const cost = firstClassTicket * 150 + economyTicket * 100;
+    document.getElementById('subTotal').innerText = '$' + cost;
+
+    const vat = cost * 0.1;
+    document.getElementById('vat').innerText = '$' + vat;
+
+    const totalCost = cost + vat;
+    document.getElementById('totalCost').innerText = '$' + totalCost;
+}
+
+// get ticket number
+function getTicketNumber(ticket) {
+    const ticketInput = document.getElementById(ticket + 'Input');
+    const newTicket = parseInt(ticketInput.value);
+    return newTicket;
 }
