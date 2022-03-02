7898db6101789fbc07fda3e46dab640ee78b8c71
SaimHasanD <saimhasan514@gmail.com>
2022-03-02 22:57:58 +0600

    alert and btn

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ function handleNumberChange(ticket, isIncrease) {
 }
 
 // calculate total cost
-function calculateTotal(){
+function calculateTotal() {
     const firstClassTicket = getTicketNumber('firstClass')
     const economyTicket = getTicketNumber('economy')
 
     const cost = firstClassTicket * 150 + economyTicket * 100;
     document.getElementById('subTotal').innerText = '$' + cost;
 
-    const vat = cost * 0.1;
+    const vat = cost * 0.1; // If the number was decimal then I would use math.round
     document.getElementById('vat').innerText = '$' + vat;
 
     const totalCost = cost + vat;
@@ -33,4 +33,15 @@ function getTicketNumber(ticket) {
     const ticketInput = document.getElementById(ticket + 'Input');
     const newTicket = parseInt(ticketInput.value);
     return newTicket;
+}
+
+// Confirming ticket with alert.
+function confirmTicket() {
+    const totalCost = document.getElementById('totalCost').innerText;
+    // const totalCost = ;
+    if (totalCost > '$'+0) {
+        alert('We confirm your ticket. We wish you a happy journey. Stay with us, Thank you.');
+    } else {
+        alert('Your ticket input is empty, Please put your ticket quantity and go for a safe journey.');
+    }   
 }
